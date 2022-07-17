73986e12bdead543bae29c98a0805cb2c4765fdb
dongree <dongwoo0307@naver.com>
2022-07-17 23:57:23 +0900

    create SignUpStep1 fragment, click listner

--- app/src/main/java/com/example/nanuer/SignUpActivity.kt
+++ app/src/main/java/com/example/nanuer/SignUpActivity.kt
@@ -2,10 +2,29 @@ package com.example.nanuer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nanuer.databinding.ActivitySignupBinding
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignupBinding
+
+    var step = 1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val fragment1 = SingUpStep1Fragment()
+        fragmentTransaction.add(R.id.singup_fragment_container, fragment1)
+        fragmentTransaction.commit()
+
+        binding.singupBackIv.setOnClickListener{
+            when(step){
+                1 -> finish()
+            }
+        }
     }
 }
