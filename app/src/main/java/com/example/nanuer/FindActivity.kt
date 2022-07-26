6ba22fb233073aa8254b6f600f5500b5b9f7cf84
Jhyun02 <hjh75035117@gmail.com>
2022-07-27 01:55:45 +0900

    timer_1

--- app/src/main/java/com/example/nanuer/FindActivity.kt
+++ app/src/main/java/com/example/nanuer/FindActivity.kt
@@ -4,26 +4,30 @@ package com.example.nanuer
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.nanuer.databinding.ActivityFindBinding
 import com.example.nanuer.databinding.FragmentFindIdStep1Binding
 import com.google.android.material.tabs.TabLayoutMediator
+import java.util.*
+import kotlin.concurrent.timer
 
-class FindActivity : AppCompatActivity(){
+class FindActivity : AppCompatActivity() {
     lateinit var binding: ActivityFindBinding
 
-    private lateinit var viewPager: ViewPager2
     private val tabTextList = arrayListOf("아이디 찾기", "비밀번호 찾기")
 
 
-    var btn : Button? = null
-    var sendbtn : Button? = null
-    var resendbtn : Button? = null
-    var check =true
+    var btn: Button? = null
+    var btn2: Button? = null
+    var check = true
+    var check2 = true
+    var time = 3
+    var timerTask: Timer? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +36,9 @@ class FindActivity : AppCompatActivity(){
         setContentView(binding.root)
 
         clickbtn()
+        clickbtn2()
 
-        binding.findDeleteLogoIv.setOnClickListener{
+        binding.findDeleteLogoIv.setOnClickListener {
             finish()
         }
 
@@ -47,36 +52,105 @@ class FindActivity : AppCompatActivity(){
         }.attach()
 
         btn = findViewById<Button>(R.id.find_id_step1_send_number_btn)
+        btn2 = findViewById<Button>(R.id.find_id_step1_okay_btn)
 
     }
 
-    fun  clickbtn(){
-        btn?.setOnClickListener{onClick(btn)}
+    fun starttimer() {
+
+        val timecheck = findViewById<TextView>(R.id.find_id_step1_timer)
+
+        timerTask = timer(period = 1000) {
+            time--
+            val min = time / 100
+            val sec = time
+
+            runOnUiThread {
+                timecheck?.text= "${sec}"// TextView 세팅
+            }
+        }
+    }
+
+//    fun laptime() {
+//
+//        val lapTime = time
+//        val lap_layout = findViewById<TextView>(R.id.find_id_step1_timer)
+//
+//        val textView = findViewById<TextView>(R.id.find_id_step1_timer).apply {
+//            setTextSize(12f)
+//
+//        }
+//
+//        textView.text = "남은시간 ${lapTime / 100}.${lapTime % 100}"
+//
+//    }
+
+
+
+    fun clickbtn() {
+        btn?.setOnClickListener { onClick(btn) }
+    }
+
+    fun clickbtn2() {
+        btn2?.setOnClickListener { onClick2(btn2) }
     }
 
 
-
-      fun onClick(v: View?){
-        when(v?.id) {
+    fun onClick(v: View?) {
+        when (v?.id) {
             R.id.find_id_step1_send_number_btn -> {
-                val tView = findViewById<TextView>(R.id.find_id_step1_send_number_btn)
+                var sendbtn = findViewById<TextView>(R.id.find_id_step1_send_number_btn)
+                var resendbtn = findViewById<Button>(R.id.find_id_step1_resend_btn)
+                var okaybtn = findViewById<Button>(R.id.find_id_step1_okay_btn)
+                var ctEt = findViewById<EditText>(R.id.find_id_step1_certification_number_et)
+                var timertv = findViewById<TextView>(R.id.find_id_step1_timer)
+                var remessagetv = findViewById<TextView>(R.id.find_id_step1_resend_message_tv)
+
+
                 if (check) {
-                    tView.setText("인증번호")
+                    sendbtn.visibility = View.VISIBLE
                     check = false
-                }
-                else{
-                    tView.setText("재전송")
-                    tView.setTextColor(ContextCompat.getColor(this,R.color.blue_light_1))
+                } else {
+                    sendbtn.visibility = View.GONE
+                    resendbtn.visibility = View.VISIBLE
                     check = true
+
+                    okaybtn.visibility = View.VISIBLE
+                    ctEt.visibility = View.VISIBLE
+                    timertv.visibility = View.VISIBLE
+                    remessagetv.visibility = View.VISIBLE
+
+                    starttimer()
+
+
+                }
+            }
+        }
+    }
+
+        fun onClick2(v2: View?) {
+            when (v2?.id) {
+                R.id.find_id_step1_okay_btn -> {
+                    var okaybtn = findViewById<Button>(R.id.find_id_step1_okay_btn)
+                    var correctbtn = findViewById<Button>(R.id.find_id_step1_correct_btn)
+
+                    if (check2) {
+                        okaybtn.visibility = View.GONE
+                        correctbtn.visibility = View.VISIBLE
+                        check2 = false
+                    } else {
+                        okaybtn.visibility = View.VISIBLE
+                        correctbtn.visibility = View.GONE
+                        check2 = true
+
+
+
+           }
                 }
             }
         }
 
+
     }
 
-
-
-
-
-}
 
